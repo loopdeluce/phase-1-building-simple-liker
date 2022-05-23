acbf723347236ec3d9c232d1262099b9015f277b
Lucy <lucy.d.bullen@gmail.com>
2022-05-23 10:03:31 -0600

    commit tree f18a9d3423e70f4d95227640057fd0de5b5ea9fc

--- main.js
+++ main.js
@@ -4,8 +4,54 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//Add hidden class to the modal so it doesn't appear when the screen is first loaded
+const error = document.getElementById('modal');
+error.className = 'hidden';
+
+// When a user clicks on an empty heart:
+// Invoke mimicServerCall to simulate making a server request
+// When the "server" returns a failure status:
+// Respond to the error using a .catch(() => {}) block after your .then(() => {}) block.
+// Display the error modal by removing the .hidden class
+// Display the server error message in the modal
+// Use setTimeout to hide the modal after 3 seconds (add the .hidden class)
+// When the "server" returns a success status:
+// Change the heart to a full heart
+// Add the .activated-heart class to make the heart appear red
 
 
+const likeGlyph = document.querySelectorAll('footer li .like-glyph');
+likeGlyph.forEach(emoji => emoji.addEventListener('click', handleHeartClick));
+
+
+function handleHeartClick(event) {
+  mimicServerCall()
+  .then(completeSuccessfulServerCall(event))
+  .catch((resp) => completeFailedServerCall(resp))
+}
+
+function completeSuccessfulServerCall (event) {
+  console.log('success')
+ // console.log('target value:', event.target.textContent)
+  if(event.target.textContent === EMPTY_HEART) {
+    event.target.textContent = FULL_HEART;
+    event.target.className = 'activated-heart';
+  }
+  else if (event.target.textContent === FULL_HEART) {
+    event.target.textContent = EMPTY_HEART;
+    event.target.classList.remove('activated-heart');
+  }
+}
+
+function completeFailedServerCall (resp) {
+  console.log(resp);
+  error.classList.remove('hidden');
+  const errorP = document.getElementById('modal-message');
+  errorP.textContent = resp;
+  setTimeout(() => error.className = 'hidden', 3000);
+}
+
+//likeGlyph.addEventListener('click', event => console.log(event));
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
